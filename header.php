2b3402b30fa1c450d0b3be441adf8255164d91d9
templeman <templeman@me.com>
2014-11-12 16:03:50 -0800

    Switch to Google Maps Javascript API v3

--- header.php
+++ header.php
@@ -1,25 +1,25 @@
 <?php
   global $post;
-  $ee 				= $post->ID;	
+  $ee         = $post->ID;
   if(!is_page('home') && !is_page('about') && !is_page('services')) :
-    $strip_head 		= get_field('hed_img',$ee);
+    $strip_head     = get_field('hed_img',$ee);
     if(!$strip_head) :
-      $strip_head 	= get_field('gh_img','Options');
+      $strip_head   = get_field('gh_img','Options');
     endif;
     $strip = wp_get_attachment_image_src($strip_head, 'full-bleed-strip');
   else :
-    $hero_img 		= get_field('hero_image',$ee);
+    $hero_img     = get_field('hero_image',$ee);
     // echo var_dump($hero_img);
     //global $hero_img;
     // $strip = wp_get_attachment_image_src($hero_img, 'full-bleed');
     $strip = wp_get_attachment_image_src($hero_img, 'full');
-    $hero_cont 		= get_field('hero_content',$ee);
-    $hero_link 		= get_field('hero_link',$ee);
-    $hero_link_lbl 	= get_field('hero_link_label',$ee);
-    //$sub_left 		= get_field('sub_hero_left',$ee);
-    //$sub_right 		= get_field('sub_hero_right',$ee);
+    $hero_cont    = get_field('hero_content',$ee);
+    $hero_link    = get_field('hero_link',$ee);
+    $hero_link_lbl  = get_field('hero_link_label',$ee);
+    //$sub_left     = get_field('sub_hero_left',$ee);
+    //$sub_right    = get_field('sub_hero_right',$ee);
   endif;
-    $bee_gee 			= $strip[0];
+    $bee_gee      = $strip[0];
     // echo var_dump($bee_gee);
 ?>
 
@@ -41,29 +41,59 @@
 
 
 </. #pdxwebmafia /. #webuildbadasswarships /.> --><head>
-	<meta charset="utf-8">
-	<title><?php wp_title(''); ?></title>
-	<meta name="viewport" content="width=device-width">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico">
-	<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png">
-	<link href='http://fonts.googleapis.com/css?family=Metrophobic' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+  <title><?php wp_title(''); ?></title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png">
+  <link href='http://fonts.googleapis.com/css?family=Metrophobic' rel='stylesheet' type='text/css'>
 
-	<?php echo '<script> var $loca = "'.URL.'"; </script>'; ?>
-	<?php wp_head(); ?>
+  <?php echo '<script> var $loca = "'.URL.'"; </script>'; ?>
+  <?php wp_head(); ?>
+  <script type="text/javascript"
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCU7Q3reQdSCnZMvlhzmFI-6Nuqz3WFd5M">
+  </script>
+  <script>
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(45.4843698,-122.6759628);
+      var mapOptions = {
+        zoom: 16,
+        center: myLatlng,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL
+        }
+      }
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'McClenahan Bruer'
+      });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+  </script>
 </head>
 
 <body <?php body_class(); ?>>
-	<!--[if lt IE 8]>
-	    <p class="whatbrowser">You are using an <strong>outdated</strong> browser. Please <a href="http://whatbrowser.org">upgrade your browser</a> or <a href="http://whatbrowser.org/">learn more</a> about your browsing options.</p>
-	<![endif]-->
+  <!--[if lt IE 8]>
+      <p class="whatbrowser">You are using an <strong>outdated</strong> browser. Please <a href="http://whatbrowser.org">upgrade your browser</a> or <a href="http://whatbrowser.org/">learn more</a> about your browsing options.</p>
+  <![endif]-->
 
-	<?php// echo $strip[0]; ?>
-	<?php //print_r($me); 
-		if(is_page('about')) : $head_ico = 'fa-info-circle'; endif;
-		if(is_page('connect')) : $head_ico = 'fa-link'; endif;
-		if(is_page('services')) : $head_ico = 'fa-gear'; endif;
-		if(is_post_type_archive('work')||is_post_type_archive('case-studies')||is_page('cs-proof')||is_page('clients')) : $head_ico = 'fa-folder'; endif;
-		?>
+  <?php// echo $strip[0]; ?>
+  <?php //print_r($me); 
+    if(is_page('about')) : $head_ico = 'fa-info-circle'; endif;
+    if(is_page('connect')) : $head_ico = 'fa-link'; endif;
+    if(is_page('services')) : $head_ico = 'fa-gear'; endif;
+    if(is_post_type_archive('work')||is_post_type_archive('case-studies')||is_page('cs-proof')||is_page('clients')) : $head_ico = 'fa-folder'; endif;
+    ?>
 <div class="full-wrap">
 
 <header class="header clearfix" 
@@ -123,18 +153,18 @@
     <?php endif; ?>
   </header>
 
-	<div class="container">
+  <div class="container">
 
-		<?php if((  is_page()  &&  !is_page('home')  &&  !is_page('services')  &&  !is_page('about')  && !is_page('blog-single-proof'))||is_archive()) : ?>
-			<div class="row">
-				<div class="twenty-four columns">
-					<h1 class="head-it clearfix"><?php 
-						if(is_page()) :
-							echo $post->post_title;
-						elseif(is_post_type_archive('work')||is_post_type_archive('case-studies')) :
-							echo 'Work';
-						endif; ?><i class="fa <?php echo $head_ico; ?>"></i><!--  --></h1>
-				</div>
-			</div>
-		<?php endif; ?>
-	<?php $me = get_post_type(); ?>
+    <?php if((  is_page()  &&  !is_page('home')  &&  !is_page('services')  &&  !is_page('about')  && !is_page('blog-single-proof'))||is_archive()) : ?>
+      <div class="row">
+        <div class="twenty-four columns">
+          <h1 class="head-it clearfix"><?php 
+            if(is_page()) :
+              echo $post->post_title;
+            elseif(is_post_type_archive('work')||is_post_type_archive('case-studies')) :
+              echo 'Work';
+            endif; ?><i class="fa <?php echo $head_ico; ?>"></i><!--  --></h1>
+        </div>
+      </div>
+    <?php endif; ?>
+  <?php $me = get_post_type(); ?>
--- page-about.php
+++ page-about.php
@@ -1,66 +1,66 @@
 <?php get_header();
 
-	$jobs = get_field('jobs');
-	$ee = $post->ID;
-	$sub_left = get_field('sub_hero_left',$ee);
-	$sub_right = get_field('sub_hero_right',$ee);
+  $jobs = get_field('jobs');
+  $ee = $post->ID;
+  $sub_left = get_field('sub_hero_left',$ee);
+  $sub_right = get_field('sub_hero_right',$ee);
 ?>
 
 
 
-	<div class="row">
-		<div class="twenty-four columns">
+  <div class="row">
+    <div class="twenty-four columns">
 
-			<?php /* <div class="float-wrap">
-				<div class="float content">
-					<h2>Dan Cruzat</h2>
-					<p>Developer, Strategist, Gentleman of Leisure</p>
+      <?php /* <div class="float-wrap">
+        <div class="float content">
+          <h2>Dan Cruzat</h2>
+          <p>Developer, Strategist, Gentleman of Leisure</p>
 
-					<div class="row">
-						<div class="columns twelve">
-							<img src="http://placehold.it/488x488" alt="">
-						</oiv>
-						<div class="columns twelve">
-							<p>Lorem ipsum dolor sit amet, te tale offendit reprimique usu, mel ea munere recusabo. Te duo nibh natum novum, eu qui simul percipitur constituam. Ne latine assentior has. Ei dignissim definitiones mei, ex viderer theophrastus quo. Ea eos reprimique repudiandae. </p>
-							<p>Ne vero option eripuit mea, vim et falli vituperatoribus. Mel vero mutat voluptatibus ex, usu et feugiat tibique intellegam, vidit dolores id pro. Et malis adipisci repudiare eam. At stet delicatissimi eum. In eros appareat molestiae usu, adipisci recusabo ea mea. Id wisi aliquid indoctum mea, iusto aliquid rationibus sed cu.</p>
-							<p>Verear liberavisse nec ex, in nihil quando platonem vim, ea quem atqui verterem sed. Te aeque sonet mandamus his. Et possit verterem mel, constituam conclusionemque cu vel. </p>
-							<p>Has ne eros interpretaris. Quo et legimus iudicabit, ex simul delenit minimum vix, ne veniam principes mea. Ex stet iudico tamquam nec. Alia nonumes molestie ad pri, sit apeirian neglegentur an.</p>
-							<p>Eu vix elit animal. An usu adhuc scripta dissentiunt. His in dicta altera vulputate, eu assum summo has. Id error omnium sed, eius primis offendit sea cu, in per postea accusam. Nam id natum omnis.</p>
+          <div class="row">
+            <div class="columns twelve">
+              <img src="http://placehold.it/488x488" alt="">
+            </oiv>
+            <div class="columns twelve">
+              <p>Lorem ipsum dolor sit amet, te tale offendit reprimique usu, mel ea munere recusabo. Te duo nibh natum novum, eu qui simul percipitur constituam. Ne latine assentior has. Ei dignissim definitiones mei, ex viderer theophrastus quo. Ea eos reprimique repudiandae. </p>
+              <p>Ne vero option eripuit mea, vim et falli vituperatoribus. Mel vero mutat voluptatibus ex, usu et feugiat tibique intellegam, vidit dolores id pro. Et malis adipisci repudiare eam. At stet delicatissimi eum. In eros appareat molestiae usu, adipisci recusabo ea mea. Id wisi aliquid indoctum mea, iusto aliquid rationibus sed cu.</p>
+              <p>Verear liberavisse nec ex, in nihil quando platonem vim, ea quem atqui verterem sed. Te aeque sonet mandamus his. Et possit verterem mel, constituam conclusionemque cu vel. </p>
+              <p>Has ne eros interpretaris. Quo et legimus iudicabit, ex simul delenit minimum vix, ne veniam principes mea. Ex stet iudico tamquam nec. Alia nonumes molestie ad pri, sit apeirian neglegentur an.</p>
+              <p>Eu vix elit animal. An usu adhuc scripta dissentiunt. His in dicta altera vulputate, eu assum summo has. Id error omnium sed, eius primis offendit sea cu, in per postea accusam. Nam id natum omnis.</p>
 
-							<ul class="social-rack tiles three_up">
-								<li><a href="javascript:alert('social links')"><i class="fa fa-twitter"></i></a></li>
-								<li><a href="javascript:alert('social links')"><i class="fa fa-linkedin"></i></a></li>
-								<li><a href="javascript:alert('social links')"><i class="fa fa-pinterest"></i></a></li>
-							</ul>
-						</div>
-					</div>
+              <ul class="social-rack tiles three_up">
+                <li><a href="javascript:alert('social links')"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="javascript:alert('social links')"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="javascript:alert('social links')"><i class="fa fa-pinterest"></i></a></li>
+              </ul>
+            </div>
+          </div>
 
-				<div class="row">
-					<div class="columns twenty-four">
-						<h3 class="head-it clearfix">Recent Posts<i class="fa fa-chat"></i></h3>
-						<div class="rac row">
+        <div class="row">
+          <div class="columns twenty-four">
+            <h3 class="head-it clearfix">Recent Posts<i class="fa fa-chat"></i></h3>
+            <div class="rac row">
 
-							<ul class="link-list">
-							<?php for($i=0;$i<4;$i++) : ?>
+              <ul class="link-list">
+              <?php for($i=0;$i<4;$i++) : ?>
 
-								<li>
-									<a href="#" class="titulo">Collective Creativity</a>
-									<span class="meta">March 11th, 2014</span>
-								</li>
-							<?php endfor; ?>
-							</ul>
+                <li>
+                  <a href="#" class="titulo">Collective Creativity</a>
+                  <span class="meta">March 11th, 2014</span>
+                </li>
+              <?php endfor; ?>
+              </ul>
 
-							</div>
-						</div>
+              </div>
+            </div>
 
-						<a href="#" class="close">×</a>
-					</div>
-				</div>
-			</div>*/ ?>
+            <a href="#" class="close">×</a>
+          </div>
+        </div>
+      </div>*/ ?>
       <div class="float-wrap">
       </div>
 
-			<div class="tucker">
+      <div class="tucker">
         <div class="row">
           <div class="twelve columns bump18">
             <?php echo apply_filters('the_content', $sub_left); ?>
@@ -70,53 +70,53 @@
           </div>
         </div>
 
-				<h3 class="head-it clearfix bump42">The Crew<i class="fa fa-users"></i></h3>
-				<div class="about content bump18">
-						<div class="rac row">
+        <h3 class="head-it clearfix bump42">The Crew<i class="fa fa-users"></i></h3>
+        <div class="about content bump18">
+            <div class="rac row">
 
-						<?php 
-							$team = get_posts(array(
-								'posts_per_page'   => -1,
-								//'offset'           => 0,
-								//'category'         => '',
-								'orderby'          => 'menu_order',
-								'order'            => 'ASC',
-								//'include'          => '',
-								//'exclude'          => '',
-								//'meta_key'         => '',
-								//'meta_value'       => '',
-								'post_type'        => 'team',
-								//'post_mime_type'   => '',
-								//'post_parent'      => '',
-								'post_status'      => 'publish' )); //,
-								//'suppress_filters' => true ));
+            <?php 
+              $team = get_posts(array(
+                'posts_per_page'   => -1,
+                //'offset'           => 0,
+                //'category'         => '',
+                'orderby'          => 'menu_order',
+                'order'            => 'ASC',
+                //'include'          => '',
+                //'exclude'          => '',
+                //'meta_key'         => '',
+                //'meta_value'       => '',
+                'post_type'        => 'team',
+                //'post_mime_type'   => '',
+                //'post_parent'      => '',
+                'post_status'      => 'publish' )); //,
+                //'suppress_filters' => true ));
 
-							//for($i=0;$i<12;$i++) :
-							foreach($team as $coun => $tmate) :
-								$d = $tmate->ID;
-								$role = get_field('role',$d);
-								//print_r($tmate);
-							 ?>
-							<a class="tile" data-id="<?php echo $d; ?>" data-slug="<?php echo $tmate->post_name; ?>">
-								<?php if(has_post_thumbnail($d)) : //$img = wp_get_attachment_image_src( get_post_thumbnail_id($d), 'full' );
-									echo get_the_post_thumbnail($d,'bl-port');
-								else : ?>
-									<img src="http://placehold.it/488x540" alt="" />
-								<?php endif; ?>
-								<div class="screen">
-								<h3><?php echo $tmate->post_title; ?></h3>
-								<p><?php echo $role; ?></p>
-								<span class="cta">Learn More</span>
-								</div>
-							</a>
-						<?php endforeach;
+              //for($i=0;$i<12;$i++) :
+              foreach($team as $coun => $tmate) :
+                $d = $tmate->ID;
+                $role = get_field('role',$d);
+                //print_r($tmate);
+               ?>
+              <a class="tile" data-id="<?php echo $d; ?>" data-slug="<?php echo $tmate->post_name; ?>">
+                <?php if(has_post_thumbnail($d)) : //$img = wp_get_attachment_image_src( get_post_thumbnail_id($d), 'full' );
+                  echo get_the_post_thumbnail($d,'bl-port');
+                else : ?>
+                  <img src="http://placehold.it/488x540" alt="" />
+                <?php endif; ?>
+                <div class="screen">
+                <h3><?php echo $tmate->post_title; ?></h3>
+                <p><?php echo $role; ?></p>
+                <span class="cta">Learn More</span>
+                </div>
+              </a>
+            <?php endforeach;
 
-							//endfor; ?>
+              //endfor; ?>
 
-					</div>
-				</div>
+          </div>
+        </div>
 
-				<h2 class="head-it clearfix">Job Openings<i class="fa fa-briefcase"></i></h3>
+        <h2 class="head-it clearfix">Job Openings<i class="fa fa-briefcase"></i></h3>
 
 
         <?php if($jobs) :
@@ -137,16 +137,16 @@
   </div>
 
 
-	<?php /* 
-	<section id="main" role="main">
+  <?php /* 
+  <section id="main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'templates/partials/content', 'page' ); ?>
+      <?php get_template_part( 'templates/partials/content', 'page' ); ?>
 
-		<?php endwhile; ?>
+    <?php endwhile; ?>
 
-	</section> <!-- /#main -->
+  </section> <!-- /#main -->
 
 get_sidebar();*/ ?>
 
