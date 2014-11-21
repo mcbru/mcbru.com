f44ea461b90fa5675565ee62a56b98110c322224
templeman <templeman@me.com>
2014-11-21 11:29:52 -0800

    Bump up font size for body copy

--- footer.php
+++ footer.php
@@ -1,32 +1,31 @@
 
 
-		<footer class='footer'>
-			<?php dynamic_sidebar( 'Footer' ); ?>
-			<p>&copy; <?php echo date( "Y" ); echo " "; //bloginfo( 'name' ); ?><a href="<?php echo site_url(); ?>">mcbru.com</a></p>
-		</footer>
+    <footer class='footer'>
+      <?php dynamic_sidebar( 'Footer' ); ?>
+      <p>&copy; <?php echo date( "Y" ); echo " "; //bloginfo( 'name' ); ?><a href="<?php echo site_url(); ?>">mcbru.com</a></p>
+    </footer>
 
-	</div> <!-- /.container -->
+  </div> <!-- /.container -->
 
-	<?php if(is_page('connect')) : ?>
-	<div class="cloak">
-		<div class="container">
-			<div class="form-pop">
-				<h3 class="head-it no-base">Connect Today</h3>
-				
-				<?php echo do_shortcode('[contact-form-7 id="4"]'); ?>
+  <?php if(is_page('connect')) : ?>
+  <div class="cloak">
+    <div class="container">
+      <div class="form-pop">
+        <h3 class="head-it no-base">Connect Today</h3>
+
+        <?php echo do_shortcode('[contact-form-7 id="4"]'); ?>
 
 
-				<a href="#" class="close">×</a>
-			</div>
-			<div class="form-confirmation">
-				
-				<img src="<?php echo THEME; ?>/assets/img/mcb-thank-you.png" alt="" />
-				<p>Glad you dropped us a line!<br>Hang tight, we will be in touch shortly.</p>
-				<a href="#" class="close">×</a>
-			</div>
-		</div>
-	</div>
-	<?php endif; ?>
+        <a href="#" class="close">×</a>
+      </div>
+      <div class="form-confirmation">
+        <img src="<?php echo THEME; ?>/assets/img/mcb-thank-you.png" alt="" />
+        <p>Glad you dropped us a line!<br>Hang tight, we will be in touch shortly.</p>
+        <a href="#" class="close">×</a>
+      </div>
+    </div>
+  </div>
+  <?php endif; ?>
 
 </div> <!-- /.full-wrap -->
   <?php wp_footer(); ?>
