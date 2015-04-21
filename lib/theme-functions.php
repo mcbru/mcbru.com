bf214671a734000f2771b037f3762ac1c9bd3f37
templeman <samuel.templeman@me.com>
2015-04-20 17:17:16 -0700

    Add Google Analytics tracking code

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -328,4 +328,22 @@ function add_hubspot_tracking_code() { ?>
       })(document,"script","hs-analytics",300000);
     </script>
   <!-- End of Async HubSpot Analytics Code -->
+<?php }
+
+
+/**
+ * Add Google Analytics tracking code
+ */
+add_action('wp_footer', 'add_google_analytics');
+function add_google_analytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-7471064-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 <?php } ?>
